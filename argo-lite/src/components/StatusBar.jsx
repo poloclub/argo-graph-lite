8eb54b1e3acfae811f346067ec0f55a99cb9065b
RSLi <robertsli@163.com>
2020-01-23 11:37:37 -0500

    Enable light mode for status bar

--- argo-lite/src/components/StatusBar.jsx
+++ argo-lite/src/components/StatusBar.jsx
@@ -7,7 +7,12 @@ import appState from "../stores";
 class StatusBar extends React.Component {
     render() {
         return (
-            <div className={classnames("bottom-status-bar")}>
+            <div
+                className={classnames("bottom-status-bar")}
+                style={{
+                    backgroundColor: appState.preferences.darkMode ? '#30404D' : '#FFFFFF'
+                }}
+            >
                 <div className={classnames("bottom-status-bar__left")}>{appState.graph.selectedNodes.length} nodes selected</div>
                 <div className={classnames("bottom-status-bar__right")}>Color by: {appState.graph.nodes.colorBy} ({appState.graph.nodes.color.scale}), Size by: {appState.graph.nodes.sizeBy} ({appState.graph.nodes.size.scale})</div>
             </div>
