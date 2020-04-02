56e8071194559e05c3434685b67d81f15effd135
RSLi <robertsli@163.com>
2020-04-02 01:31:29 -0400

    Turn off stats debugging panel.

--- argo-lite/src/graph-frontend/src/imports.js
+++ argo-lite/src/graph-frontend/src/imports.js
@@ -15,7 +15,7 @@ var ee = require("event-emitter");
 exports.default = {
   THREE: THREE,
   STATS: STATS, // stats.js panel for showing fps and other stats.
-  STATS_SHOW: true,
+  STATS_SHOW: false,
   Edge: Edge,
   Node: Node,
   OrbitControls: OrbitControls,
