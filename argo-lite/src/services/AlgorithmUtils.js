5e13d02110be0f19b1f43412108c167ded858442
FZ2000 <40145076+FZ2000@users.noreply.github.com>
2020-05-10 20:32:39 -0700

    Add Additional Graph Algo (#15)
    Co-authored-by: Frank Zhou <zzhou406@gatech.edu>

--- argo-lite/src/services/AlgorithmUtils.js
+++ argo-lite/src/services/AlgorithmUtils.js
@@ -34,7 +34,24 @@ export function convertToJSGraph(snapshot) {
     snapshot.rawGraph.edges.forEach(e => {
         g.addEdge(idDict[e.source_id], idDict[e.target_id]);
     });
-    return new jsgraphs.ConnectedComponents(g);
+    return [g, idDict];
+}
+
+/**
+ * Convert Argo-lite snapshot for use in the js-graph-algorithms library (Weighted).
+ * @param {*} snapshot Argo-lite Snapshot Object exported by GraphStore
+ */
+export function convertToJSGraphWeightedDi(snapshot) {
+    var idDict = {};
+    var i;
+    for (i = 0; i < snapshot.rawGraph.nodes.length; i++) {
+        idDict[snapshot.rawGraph.nodes[i].id] = i;
+    }
+    var g = new jsgraphs.WeightedDiGraph(snapshot.rawGraph.nodes.length);
+    snapshot.rawGraph.edges.forEach(e => {
+        g.addEdge(new jsgraphs.Edge(idDict[e.source_id], idDict[e.target_id], 1.0));
+    });
+    return [g, idDict];
 }
 
 /**
@@ -61,7 +78,51 @@ export function averageClusteringCoefficient(snapshot) {
  * @param {*} rawGraph the rawGraph inside appState
  */
 export function connectedComponents(snapshot) {
-    var cc = convertToJSGraph(snapshot);
+    var cc = new jsgraphs.ConnectedComponents(convertToJSGraph(snapshot)[0]);
     return cc.componentCount();
+}
+
+/**
+ * Calculate the density of a graph
+ * @param {*} rawGraph the rawGraph inside appState
+ */
+export function graphDensity(snapshot) {
+    const nodeCount = snapshot.rawGraph.nodes.length;
+    const edgeCount = snapshot.rawGraph.edges.length / 2;
+    return (2 * edgeCount) / ((nodeCount) * (nodeCount - 1));
+}
+
+/**
+ * Calculate the average degree of a graph
+ * @param {*} rawGraph the rawGraph inside appState
+ */
+export function averageDegree(snapshot) {
+    let sum = 0;
+    snapshot.rawGraph.nodes.forEach(e => {
+            sum += e.degree;
+        }
+    )
+    return sum / snapshot.rawGraph.nodes.length;
+}
+
+/**
+ * Calculate the diameter of a graph
+ * @param {*} rawGraph the rawGraph inside appState
+ */
+export function exactGraphDiameter(snapshot) {
+    let temp = convertToJSGraphWeightedDi(snapshot);
+    let jsg = temp[0];
+    let idDict = temp[1]
+    let dia = -1;
+    snapshot.rawGraph.nodes.forEach(e => {
+        let dijkstra = new jsgraphs.Dijkstra(jsg, idDict[e.id]);
+        snapshot.rawGraph.nodes.forEach(f => {
+            if(dijkstra.hasPathTo(idDict[f.id])){
+                let pathLength = dijkstra.pathTo(idDict[f.id]).length;
+                dia = Math.max(dia, pathLength);
+            }
+        })
+    });
+    return dia;
 }
  
