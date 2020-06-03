1453226ab48479f5a83f4b72a4d392ca38065305
RSLi <robertsli1998@gmail.com>
2020-06-02 21:25:42 -0700

    Improve NodeTable sorting.

--- argo-lite/src/components/NodeTable.jsx
+++ argo-lite/src/components/NodeTable.jsx
@@ -17,7 +17,8 @@ class NodeTable extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            sortBy: 'pagerank'
+            sortBy: 'pagerank',
+            sortOrder: 'descending', // or 'ascending'
         };
     }
 
@@ -25,8 +26,24 @@ class NodeTable extends React.Component {
     let filteredNodes = this.props.nodes;
     
     filteredNodes.sort((n1, n2) => {
-        if (n1[this.state.sortBy] && n2[this.state.sortBy]) {
-            return n2[this.state.sortBy] - n1[this.state.sortBy];
+        const n1val = n1[this.state.sortBy];
+        const n2val = n2[this.state.sortBy];
+        if (n1val && n2val) {
+            // Check if these are numerical fields, if so sort by number.
+            const n1valAsNum = Number(n1val);
+            const n2valAsNum = Number(n2val);
+            if (!Number.isNaN(n1valAsNum) && !Number.isNaN(n2valAsNum)) {
+                // both values are numeric, compare by their value.
+                if (this.state.sortOrder === 'descending') {
+                    return n2valAsNum - n1valAsNum;
+                }
+                return n1valAsNum - n2valAsNum;
+            } else {
+                // not all values are numeric, sort by string order.
+                const n1valAsString = n1val.toString();
+                const n2valAsString = n2val.toString();
+                return (this.state.sortOrder === 'descending' ? -1 : 1) * n1valAsString.localeCompare(n2valAsString);
+            }
         }
         return 0;
     });
@@ -41,6 +58,13 @@ class NodeTable extends React.Component {
                     value={this.state.sortBy}
                     onSelect={(selected) => {
                         this.setState({sortBy: selected});
+                    }}
+                />
+                <SimpleSelect
+                    items={['descending', 'ascending']}
+                    value={this.state.sortOrder}
+                    onSelect={(selected) => {
+                        this.setState({sortOrder: selected});
                     }}
                 />
                 <table className="argo-table-container__table pt-table pt-bordered pt-striped">
