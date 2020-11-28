2623f0348585e4152d8209bcf4a901af3ad5581b
Li, Kevin Y <kevin.li@gatech.edu>
2020-11-28 18:03:16 -0500

    Update EdgesPanel.jsx

--- argo-lite/src/components/panels/EdgesPanel.jsx
+++ argo-lite/src/components/panels/EdgesPanel.jsx
@@ -63,6 +63,22 @@ class EdgesPanel extends React.Component {
                     </div>
                 </Collapsable>
 
+                <div>
+                    <section>
+                        <p style={{display: "inline"}}>Directed Graph?: </p>
+                        <div style={{display: "inline", float: "right"}}>
+                              <input 
+                                type="checkbox"
+                                onChange={it => {
+                                    console.log(appState.graph.directedOrNot);
+                                    appState.graph.directedOrNot = !appState.graph.directedOrNot;
+                                }
+                                }
+                              />
+                        </div>
+                    </section>
+                </div>
+                
                 {/* Collapsable Option: Thickness */}
                 {/* <Collapsable
                     name="Thickness"
